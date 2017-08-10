765db092583237dca577918f584252539d8b48ac
David Kennedy <davidken@live.ie>
2017-08-10 23:36:10 +0100

    Add project files.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject targetObject;
+    // This is used to offset the camera such that it keeps ahead of the  
+    // mouse, else the mouse will just move to the center of the screen
+    private float distanceToTarget;   
+
+    // Use this for initialization
+    void Start () {
+        // Calculate the amout of offset the camera 
+        distanceToTarget = transform.position.x - targetObject.transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float targetObjectX = targetObject.transform.position.x;
+
+        Vector3 newCameraPosition = transform.position;
+        newCameraPosition.x = targetObjectX + distanceToTarget;
+        transform.position = newCameraPosition;
+
+    }
+}
---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+    public float jetpackForce = 75f;
+    public float forwardMovementSpeed = 3.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+    /// <summary>
+    /// This is called on every frame
+    /// </summary>
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// This is called at a fixed rate
+    /// </summary>
+    void FixedUpdate()
+    {
+        bool jetpackActive = Input.GetButton("Fire1");
+
+        if (jetpackActive)
+        {
+            //rigidbody2D.AddForce(new Vector2(0, jetpackForce));
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jetpackForce));
+        }
+
+        Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+        newVelocity.x = forwardMovementSpeed;
+        GetComponent<Rigidbody2D>().velocity = newVelocity;
+    }
+}
