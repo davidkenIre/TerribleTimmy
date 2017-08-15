80c362ad7a07e1a0a7d03776643d553a69a2651e
David Kennedy <davidken@live.ie>
2017-08-15 21:06:27 +0100

    Added animations and lasers

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class MouseController : MonoBehaviour {
     public float jetpackForce = 75f;
     public float forwardMovementSpeed = 3.0f;
+    public Transform groundCheckTransform;
+    private bool grounded;
+    public LayerMask groundCheckLayerMask;
+    Animator animator;
+    public ParticleSystem jetpack;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        animator = GetComponent<Animator>();
+    }
 	
     /// <summary>
     /// This is called on every frame
@@ -34,5 +39,23 @@ public class MouseController : MonoBehaviour {
         Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
         newVelocity.x = forwardMovementSpeed;
         GetComponent<Rigidbody2D>().velocity = newVelocity;
+
+        UpdateGroundedStatus();
+        AdjustJetpack(jetpackActive);
+    }
+
+    void UpdateGroundedStatus()
+    {
+        //1
+        grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+
+        //2
+        animator.SetBool("grounded", grounded);
+    }
+
+    void AdjustJetpack(bool jetpackActive)
+    {
+        jetpack.enableEmission = !grounded;
+        jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f;
     }
 }
